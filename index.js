c7e7027be69e47d352fb78a46dfe0034eedbd7cb
michaelzhong168 <michaelzhong168@gmail.com>
2023-03-16 18:12:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import Router from "koa-router";
 // https://platform.openai.com/docs/api-reference/images
 
 const configuration = new Configuration({
-    organization: process.env.APP_ORG,
+//     organization: process.env.APP_ORG,
     apiKey: process.env.APP_KEY,
 });
 const openai = new OpenAIApi(configuration);
@@ -19,15 +19,24 @@ router.get("/chat", async (ctx, next) => {
     // 获取请求中的参数
     const { prompt } = ctx.request.query;
 
-    const res = await openai.createCompletion({
-        // 对话模型
-        model: "text-davinci-003",//  text-davinci-003 对话模型
-        prompt: prompt,
-        max_tokens: 2048,
-        temperature: 0.2
-    })
+//     const res = await openai.createCompletion({
+//         // 对话模型
+//         model: "text-davinci-003",//  text-davinci-003 对话模型
+//         prompt: prompt,
+//         max_tokens: 2048,
+//         temperature: 0.2
+//     })
+    // gpt-3.5-turbo
+// replace .createCompletion() with .createChatCompletion()
+    const completion = await openai.createChatCompletion({ 
+             model: "gpt-3.5-turbo",
+// replace prompt with messages and set prompt as content with a role.
+             messages: [{role: "user", content: prompt}], 
+             temperature: 0.2
+        });
+    
     // 将生成的内容返回给客户端
-    ctx.body = res.data.choices
+    ctx.body = res.data.choices[0].message.content
 });
 
 router.get("/image", async (ctx, next) => {
